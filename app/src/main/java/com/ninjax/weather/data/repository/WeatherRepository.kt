ec8cc330ab01ad49dfbe6fc719b44e405a223f6d
doan.bien <doan.bien@asiantech.vn>
2020-04-14 15:52:39 +0700

    rm not need

--- app/src/main/java/com/ninjax/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/ninjax/weather/data/repository/WeatherRepository.kt
@@ -1,13 +1,13 @@
 package com.ninjax.weather.data.repository
 
 import com.ninjax.weather.data.WeatherApi
-import com.ninjax.weather.data.source.remote.SafeApi
 import com.ninjax.weather.data.source.remote.ResultWrapper
+import com.ninjax.weather.data.source.remote.SafeApi
 import com.ninjax.weather.data.vo.Weather
 
 class WeatherRepository(private val weatherApi: WeatherApi) : SafeApi() {
 
-    suspend fun getWeather(): ResultWrapper<Weather> = safeApiCall() {
+    suspend fun getWeather(): ResultWrapper<Weather> = safeApiCall {
         weatherApi.getWeather()
     }
 }
